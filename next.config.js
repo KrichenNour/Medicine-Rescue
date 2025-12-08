2d62ecd2c2dff646b337ccbfbb99962de7022982
DhiaElhak-Ezzeddini <dhiaelhak.ezzeddini@supcom.tn>
2025-12-08 01:20:55 +0100

    Configure Next.js for ArcGIS CSS and ESM modules

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'ui-avatars.com'],
   },
+  env: {
+    NEXT_PUBLIC_ARCGIS_API_KEY: process.env.NEXT_PUBLIC_ARCGIS_API_KEY,
+  },
 }
 
-export default nextConfig
+export default nextConfig;
